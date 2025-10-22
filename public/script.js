fe01ea96a89e0129a20e2655779b11d3a09ca526
krishj17 <krishjariwalae@gmail.com>
2025-10-22 19:37:43 +0530

    done ejs, bookmarks, next/previous

--- public/script.js
+++ public/script.js
@@ -1,1 +1,149 @@
-console.log("hello")
+// Search Section:-------------------------------------------------------------------------------
+const searchBar = document.querySelector(".search-input");
+const searchBtn = document.getElementById("searchBtn");
+const searchResults = document.getElementById("searchResults");
+
+async function handleSearch() {
+    const SearchVal = searchBar.value;
+    searchResults.innerHTML = "";   // Clear old results
+    const response = await fetch("http://localhost:5050/search", {  // Fetch search result from backend
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({Search : SearchVal}),
+    })
+    const data = await response.json();     console.log("From Server",data);
+    
+    if(data.omdb.Response=="True"){
+        for(record of data.omdb.Search){  // Repopulate the search results 
+            const div = document.createElement("div");
+            div.classList.add("search-result-item");
+            div.innerHTML = `
+                <img src="${record.Poster}" alt="${record.Title}">
+                <div class="info">
+                    <h6>${record.Title}</h6>
+                    <small>${record.Year}</small>
+                </div>
+            `;
+            searchResults.appendChild(div);
+        }
+    } else {    // if no results are found.
+        const div = document.createElement("div");
+        div.innerHTML=`<p>No Movies/Series Found!<p>`;
+        searchResults.appendChild(div);
+    }
+    searchResults.classList.add("show"); // Show dropdown
+}
+searchBtn.addEventListener("click", handleSearch)
+
+// Hide when clicking outside
+document.addEventListener("click", (e) => {
+    if (!searchResults.contains(e.target) && e.target !== searchBtn) {
+        searchResults.classList.remove("show");
+    }
+});
+
+
+
+// Load Data:---------------------------------------------------------------------------------------
+// Load Trending Data:----
+document.addEventListener("DOMContentLoaded", () => {
+    const page = document.body.dataset.page; // set in EJS <body data-page="trending">
+
+    if (page === "trending") loadTrending(1);
+    if (page === "popular") loadPopular();
+    if (page === "bookmarks") loadBookmarks();
+});
+async function loadTrending(page){
+    const response = await fetch(`/api/trending?page=${page}`);
+    const data = await response.json();
+    console.log("Trending data received from backend:", data);
+    const results = data.data.tmdbdata.results;
+    // console.log("Data results:", data.data.tmdbdata.results);
+    populateCards(results);
+}
+
+
+// Populate Cards and Handle Bookmarks:-----------------------------------------------------------
+const cardContainer = document.querySelector("#cardContainer");
+function populateCards(results) {
+    const savedBookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+    results.forEach(record => {
+        const div = document.createElement("div");
+        div.classList.add("col-6", "col-md-3", "col-xl-2", "cardCont");
+
+        const isBookmarked = savedBookmarks.includes(String(record.id));
+        const title = record.original_title || record.original_name || "Untitled";
+        const date = (record.release_date!="undefined" ? (new Date(record.release_date).getFullYear()) : "N/A") || (record.first_air_date!="undefined" ? (new Date(record.first_air_date).getFullYear()) : "N/A");
+        div.innerHTML = `
+            <div class="card" style="min-width: 9rem;">
+                <a href="#">
+                    <img src="${"https://image.tmdb.org/t/p/w500"+record.poster_path}" class="card-img-top" alt="${title}">
+                </a>
+                <div class="card-body p-1">
+                    <h5 class="card-title">${title}</h5>
+                    <span class="card-text d-flex justify-content-between">
+                        <span>${record.media_type}</span>
+                        <span>${date}</span>
+                        <button class="btn btn-default p-0 bookmark-btn" data-id="${record.id}">
+                            <i class="${isBookmarked ? 'fa-solid' : 'fa-regular'} fa-bookmark"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        `;
+
+        // Handle Bookmark Button Click
+        const btn = div.querySelector(".bookmark-btn");
+        const icon = btn.querySelector("i");
+        btn.addEventListener("click", e => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const movieId = btn.dataset.id;
+        const index = savedBookmarks.indexOf(movieId);
+
+        if (index > -1) {
+            savedBookmarks.splice(index, 1);
+            icon.classList.replace("fa-solid", "fa-regular");
+        } else {
+            savedBookmarks.push(movieId);
+            icon.classList.replace("fa-regular", "fa-solid");
+        }
+
+        localStorage.setItem("bookmarks", JSON.stringify(savedBookmarks));
+        });
+
+        cardContainer.appendChild(div);
+    });
+}
+
+
+
+
+// Handle Previous and Next Buttons:-----------------------------------------------------------
+const prevBtn = document.querySelector(".PCN .Previous");
+const currentBtn = document.querySelector(".PCN .Current");
+const nextBtn = document.querySelector(".PCN .Next");
+function CheckWebpage(newPage){  // Check which load function to call based on current webpage.
+    const page = document.body.dataset.page; 
+    if (page === "trending") loadTrending(newPage);
+    if (page === "popular") loadPopular(newPage);
+    if (page === "bookmarks") loadBookmarks(newPage);
+}
+nextBtn.addEventListener("click", ()=>{
+    let currPage = parseInt(currentBtn.textContent);
+    console.log("current page from button", currPage);
+    currPage+=1;
+    currentBtn.textContent = currPage;
+    cardContainer.innerHTML=""; // Clear old cards
+    CheckWebpage(currPage);
+});
+prevBtn.addEventListener("click", ()=>{
+    let currPage = parseInt(currentBtn.textContent);
+    console.log("current page from button", currPage);
+    currPage-=1;
+    currentBtn.textContent = currPage;
+    cardContainer.innerHTML=""; // Clear old cards
+    CheckWebpage(currPage);
+});
--- server.js
+++ server.js
@@ -3,21 +3,47 @@ dotenv.config();
 import express, { json } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import ejs from 'ejs';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+console.log("file name:- ",__filename);
+const __dirname = path.dirname(__filename);
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+// Middlewares:-------------
 app.use(cors())
 app.use(express.json())
+// Serve static files (CSS, images, JS)
+app.use(express.static(path.join(__dirname, "public")));
 
+// Routes to handle ejs:----------------------------------------------------------------------------
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home | PixelatedMovi" });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About | PixelatedMovi" });
+});
+app.get("/trending", (req, res) => {
+  res.render("trending", { title: "Trending | PixelatedMovi" });
+});
+app.get("/latest", (req, res) => {
+  res.render("latest", { title: "Latest | PixelatedMovi" });
+});
+
+
+// Search route to handle search requests
 app.post("/search", async (req, res)=>{
     try{
         const searchVal = req.body.Search;     // console.log("searched value: ", searchVal);
-
         const response = await fetch(`https://www.omdbapi.com/?s=${searchVal}&apikey=${process.env.OMDB_KEY}`)
         const data = await response.json();     // console.log("OMDB data: ", data);
-
-        res.json({status: "success", searched: searchVal, omdb: data})
+        res.status(200).json({status: "success", searched: searchVal, omdb: data})
     } catch(error){
         console.log("Error in /search route:", error);
         res.status(500).json({status:"error", message: "Internal Server Error"});
@@ -25,6 +51,18 @@ app.post("/search", async (req, res)=>{
     
 })
 
+// API routes to fetch data from TMDB
+app.get("/api/trending", async (req, res)=>{
+    try{
+        const page=req.query.page || 1;
+        const response = await fetch(`https://temp-tmdb-proxy.vercel.app/tmdb/trending?page=${page}`);
+        const data = await response.json();
+        res.status(200).json({status: "success", data: data});
+    } catch(error){
+        console.log("Error in /api/trending route:", error);
+        res.status(500).json({status:"error", message: "Internal Server Error"});
+    }
+});
 
 app.listen(PORT, ()=>{
     console.log(`App live at : http://localhost:${PORT}`);
